82ec28faf6edf01f04b9ced9c4ccaf2a245e13f1
julcalde <julcalde@student.42heilbronn.de>
2025-06-05 00:42:09 +0200

    first stage structure. In hope, I did not miss necessary things

--- cub3d.h
+++ cub3d.h
@@ -30,8 +30,73 @@
 # define MAGENTA    "\033[35m"
 # define CYAN       "\033[36m"
 
-/* ----- Struct ----- */
+/* ----- Structs ----- */
 
+typedef struct s_texture
+{
+	void	*image;
+	void	*data;
+	int		width;
+	int		heigth;
+	int		bpp;
+	int		size_line;
+	int		endian;
+}	t_texture;
+
+typedef struct s_map
+{
+	char	**grid;
+	int		width;
+	int		heigth;
+	char	*no_texture_path;
+	char	*so_texture_path;
+	char	*we_texture_path;
+	char	*ea_texture_path;
+	int		floor_color;
+	int		ceiling_color;
+}	t_map;
+
+typedef struct s_vector
+{
+	double	x;
+	double	y;
+}	t_vector;
+
+typedef struct s_ray
+{
+	t_vector	pos;
+	t_vector	dir;
+	t_vector	delta_dist;
+	t_vector	side_dist;
+	t_vector	step;
+	int			hit;
+	int			side;
+	int			map_x;
+	int			map_y;
+	double		perp_wall_dist;
+} t_ray;
+
+typedef struct s_player
+{
+	t_vector	pos;
+	t_vector	dir;
+	t_vector	plane;
+}	t_player;
+
+typedef struct s_game
+{
+    void		*mlx;
+    void		*window;
+    void		*image;
+    void		*image_data;
+    int			bpp;
+    int			size_line;
+    int			endian;
+    t_player	player;
+	t_map		map;
+	t_texture	textures[4];
+	int			keys[256];
+}	t_game;
 
 /* ----- Functions ----- */
 
