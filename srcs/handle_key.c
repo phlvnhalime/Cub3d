bd5daef405cd2402c5a8b8a2db60e2c38fb3c9ea
Halime Pehlivan <halimephlvn@icloud.com>
2025-07-09 17:54:52 +0200

    [TEST]: There are many thing we have to do. But right now it is working

--- srcs/handle_key.c
+++ srcs/handle_key.c
@@ -6,7 +6,7 @@
 /*   By: hpehliva <hpehliva@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 08:58:35 by hpehliva          #+#    #+#             */
-/*   Updated: 2025/07/09 17:10:33 by hpehliva         ###   ########.fr       */
+/*   Updated: 2025/07/09 17:53:02 by hpehliva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,12 +107,12 @@ void handle_key(mlx_key_data_t keys, void *frame)
          else if(keys.key == RIGHT)
         {
             double old_i = game->player.dir_x;
-            game->player.dir_x = game->player.dir_x * cos(-rotation_speed) - game->player.dir_y * sin(-rotation_speed);
-            game->player.dir_y = old_i * sin(-rotation_speed) + game->player.dir_y * cos(-rotation_speed);
+            game->player.dir_x = game->player.dir_x * cos(rotation_speed) - game->player.dir_y * sin(rotation_speed);
+            game->player.dir_y = old_i * sin(rotation_speed) + game->player.dir_y * cos(rotation_speed);
             // Camera plane
             double old_plane_i = game->player.plane_x;
-            game->player.plane_x = game->player.plane_x * cos(-rotation_speed) - game->player.plane_y * sin(-rotation_speed);
-            game->player.plane_y = old_plane_i * sin(-rotation_speed) + game->player.plane_y * cos(-rotation_speed);
+            game->player.plane_x = game->player.plane_x * cos(rotation_speed) - game->player.plane_y * sin(rotation_speed);
+            game->player.plane_y = old_plane_i * sin(rotation_speed) + game->player.plane_y * cos(rotation_speed);
 
         }
     }
