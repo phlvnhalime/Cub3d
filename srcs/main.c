055c03dc69b8704206c407eeeed2d1242390d2f9
julcalde <julcalde@student.42heilbronn.de>
2025-07-13 14:38:41 +0200

    [NORM FIX] Norminette approves.

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hpehliva <hpehliva@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 11:49:13 by hpehliva          #+#    #+#             */
-/*   Updated: 2025/07/12 16:36:53 by hpehliva         ###   ########.fr       */
+/*   Updated: 2025/07/13 14:38:09 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,27 @@
 
 void	close_window(void *frame)
 {
-	t_game *game = (t_game *)frame;
-	DEBUG_PRINT(YLW"Window close required\n"RST); 
+	t_game	*game;
+
+	game = (t_game *)frame;
+	DEBUG_PRINT(YLW"Window close required\n"RST);
 	garbco_game(game);
 	mlx_close_window(game->mlx);
 }
 
 int	main(int ac, char **av)
 {
-	t_game game;
+	t_game	game;
 
-	// If is it successful, we can now initialize the game window
 	if (!validate_args(ac, av))
 		return (EXIT_FAILURE);
-	
 	init_game(&game);
-	
-	/*Parse arguments and set up game*/
 	if (!parse_file(&game, av[1]))
 	{
 		return (EXIT_FAILURE);
 	}
-
 	DEBUG_PRINT(GRN"Starting game loop... \n"RST);
 	game_loop(&game);
 	garbco_game(&game);
 	return (EXIT_SUCCESS);
 }
-
