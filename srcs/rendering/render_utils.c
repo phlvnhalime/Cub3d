1cf2b4a021f316f0b972ed312d19b126acc46107
julcalde <julcalde@student.42heilbronn.de>
2025-07-13 14:58:28 +0200

    [NORM FIX] Norminette approves

--- srcs/rendering/render_utils.c
+++ srcs/rendering/render_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hpehliva <hpehliva@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 16:00:42 by hpehliva          #+#    #+#             */
-/*   Updated: 2025/07/12 16:00:58 by hpehliva         ###   ########.fr       */
+/*   Updated: 2025/07/13 14:52:56 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,70 @@
 
 void	render_frame(void *frame)
 {
-	t_game *game = (t_game *)frame;
-	// Clean the screen for black background
-	// memset(game->img->pixels... )
+	t_game	*game;
+
+	game = (t_game *)frame;
 	raycast(game);
 	DEBUG_PRINT(GRN"Frame rendered\n"RST);
+}
+
+uint32_t	get_rgba_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+	return ((r << 24) | (g << 16) | (b << 8) | a);
+}
+
+uint32_t	get_tex_color(mlx_texture_t *texture, int tex_x, int tex_y)
+{
+	uint8_t	*pix;
+	uint8_t	r;
+	uint8_t	g;
+	uint8_t	b;
+	uint8_t	a;
+
+	if (!texture || tex_x < 0 || tex_x >= (int)texture->width || \
+		tex_y < 0 || tex_y >= (int)texture->height)
+		return (0xFF00FFFF);
+	pix = texture->pixels;
+	r = pix[(tex_y * (int)texture->width + tex_x) \
+		* texture->bytes_per_pixel];
+	g = pix[(tex_y * (int)texture->width + tex_x) \
+		* texture->bytes_per_pixel + 1];
+	b = pix[(tex_y * (int)texture->width + tex_x) \
+		* texture->bytes_per_pixel + 2];
+	if (texture->bytes_per_pixel == 4)
+		a = pix[(tex_y * (int)texture->width + tex_x) \
+			* texture->bytes_per_pixel +3];
+	else
+		a = 255;
+	return (get_rgba_color(r, g, b, a));
+}
+
+int	get_wall_direction(t_ray *ray)
+{
+	if (ray->side == 0)
+	{
+		if (ray->ray_dir_x > 0)
+			return (3);
+		else
+			return (2);
+	}
+	else
+	{
+		if (ray->ray_dir_y > 0)
+			return (1);
+		else
+			return (0);
+	}
+}
+
+double	get_wall_x(t_game *game, t_ray *ray)
+{
+	double	wall_x;
+
+	if (ray->side == 0)
+		wall_x = game->player.y + ray->perp_wall_dist * ray->ray_dir_y;
+	else
+		wall_x = game->player.x + ray->perp_wall_dist * ray->ray_dir_x;
+	wall_x = wall_x - floor(wall_x);
+	return (wall_x);
 }
