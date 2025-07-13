2f1315c216302651a2006a7775cc2ca23344ec61
Halime Pehlivan <halimephlvn@icloud.com>
2025-07-13 12:34:34 +0200

    test

--- srcs/init/init.c
+++ srcs/init/init.c
@@ -6,72 +6,83 @@
 /*   By: hpehliva <hpehliva@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 14:12:07 by hpehliva          #+#    #+#             */
-/*   Updated: 2025/07/12 15:29:12 by hpehliva         ###   ########.fr       */
+/*   Updated: 2025/07/13 12:07:45 by hpehliva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-void	init_data(t_game *data)
+static void	init_garbage_and_mlx(t_game *data)
 {
-	// First i can start with the function garbage
 	garbco_init(&data->garbco);
-	/*
-		initialized img and mlx to NULL
-	*/
 	data->mlx = NULL;
 	data->img = NULL;
-	// Initialize player position
+}
+
+static void	init_player_data(t_game *data)
+{
 	data->player.x = 0.0;
 	data->player.y = 0.0;
 	data->player.dir_x = 0.0;
-	data->player.dir_y = -1.0;   // Facing down the Y-axis
-	data->player.plane_x = 0.66; // Camera plane X component
+	data->player.dir_y = -1.0;
+	data->player.plane_x = 0.66;
 	data->player.plane_y = 0.0;
 	data->player.spawn_dir = 0.0;
-	// Initialize map
+}
+
+static void	init_map_and_textures(t_game *data)
+{
+	int	i;
+
 	data->map.grid = NULL;
 	data->map.width = 0;
 	data->map.height = 0;
-	// initialize textures
-	for (int i = 0; i < 4; i++)
+	i = 0;
+	while (i < 4)
 	{
 		data->textures[i].texture = NULL;
 		data->textures[i].path = NULL;
+		i++;
 	}
-	// Initialize colors
+}
+
+static void	init_colors_and_flags(t_game *data)
+{
 	data->floor_color.r = 0;
 	data->floor_color.g = 0;
 	data->floor_color.b = 0;
 	data->ceiling_color.r = 0;
 	data->ceiling_color.g = 0;
 	data->ceiling_color.b = 0;
-	// Initialized texture
 	data->texture_count = 0;
 	data->color_count = 0;
-	data->map_started = 0; // Flag to indicate if the map has started
-	data->map_ended = 0;   // Flag to indicate if the map has ended
-	data->map_valid = 0;   // Flag to indicate if the map is valid
-	data->player_found = 0; // Flag to indicate
+	data->map_started = 0;
+	data->map_ended = 0;
+	data->map_valid = 0;
+	data->player_found = 0;
+}
+
+void	init_data(t_game *data)
+{
+	init_garbage_and_mlx(data);
+	init_player_data(data);
+	init_map_and_textures(data);
+	init_colors_and_flags(data);
 }
 
 void	init_game(t_game *game)
 {
 	init_data(game);
-	// Initialize MLX42
 	game->mlx = mlx_init(WIDTH, HEIGHT, WIN_TITLE, true);
 	if (!game->mlx)
-	{
 		mlx_error("mlx_init");
-	}
-	// Initialize the game image
 	game->img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
 	if (!game->img)
 	{
 		mlx_terminate(game->mlx);
 		mlx_error("mlx_new_image");
 	}
-	if(mlx_image_to_window(game->mlx, game->img, 0, 0) < 0)
+	if (mlx_image_to_window(game->mlx, game->img, 0, 0) < 0)
 	{
 		mlx_delete_image(game->mlx, game->img);
 		mlx_terminate(game->mlx);
